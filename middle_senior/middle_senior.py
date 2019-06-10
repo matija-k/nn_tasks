184464b73b3ba4947aabc82138b44b1056845f27
Matija Knežević <frankenstein138@gmail.com>
2019-06-11 01:51:22 +0200

    final commit

--- middle_senior/middle_senior.py
+++ middle_senior/middle_senior.py
@@ -4,32 +4,29 @@
 # Author: Matija Knezevic
 # Version: 1.0
 
-
 trademark_symbol = "®"
 
-
 # get keywords into list
-
 keywords_list = []
-with open("Keywords.txt", "r", encoding="utf-8") as file:
-	for keyword in file:
+with open("Keyword.txt", "r", encoding="utf-8") as keywords_file:
+	for keyword in keywords_file:
+		item = keyword.strip("\r\n")
+		if len(item) > 0:
+			keywords_list.append(item)
 
-		# no new line
-		current_place = keyword[:-1]
-		keywords_list.append(current_place)
-
+# remove duplicates
+final_list = list(dict.fromkeys(keywords_list)) 
 
 # read input file
-with open("acme_docs.txt", "r", encoding="utf-8") as file:
-	docs = file.read()
+with open("Acme_docs.txt", "r", encoding="utf-8") as acme_file:
+	docs = acme_file.read()
 
-# replace keywords
-for keyword in keywords_list:
+# replace keywords and write changes
+for keyword in final_list:
 	docs = docs.replace(keyword, keyword + trademark_symbol)
-	with open("acme_docs.txt", "w", encoding="utf-8") as file:
-		file.write(docs)
+	with open("Output_file.txt", "w", encoding="utf-8") as output_file:
+		output_file.write(docs)
 
 
 
 
-		
